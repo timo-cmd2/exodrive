e87e1da22b7c61ff857470fb655f0c630277bfd9
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-03-09 15:55:30 +0100

    added init shell script

---
+++ script/bootstrap.sh
@@ -0,0 +1,3 @@
+sudo apt-get install -y build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev wget
+sudo apt-get install -y pip3
+sudo apt-get install -y software-properties-common
